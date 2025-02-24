6f2c38cd19cb6343f2c830c4892f6f9a30473514
Saadkhaan12 <saadkkhan321@gmail.com>
2025-02-24 17:32:24 +0500

    Update main-chgp.js

--- plugins/main-chgp.js
+++ plugins/main-chgp.js
@@ -1,6 +1,6 @@
 let handler = async m => m.reply(`𝙁𝙊𝙇𝙇𝙊𝙒 𝙏𝙃𝙀 𝘾𝙃𝘼𝙉𝙉𝙀𝙇 𝙏𝙊 𝙂𝙀𝙏 𝙇𝘼𝙏𝙀𝙎𝙏 𝙋𝙍𝙄𝙉𝘾𝙀 𝙈𝘿 𝙐𝙋𝘿𝘼𝙏𝙀𝙎 
 
-*${mssg.link}*: https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11
+*${mssg.link}*: 
 
 *𝙊𝙒𝙉𝙀𝙍*
 ${developer}
